fa4f325947816ed58f3385c50610aa9796427b4b
RelapseGFX <albion.metollii@gmail.com>
2025-05-09 13:47:29 +0200

    add git

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/animation.js');
     eleventyConfig.addPassthroughCopy('./src/carousel.js');
     eleventyConfig.addPassthroughCopy('./src/script.js');
+    eleventyConfig.addPassthroughCopy('./src/projects.js');
 
     return {
         dir: {
