e0790a9f8c85dc1cc77dfb779c115f002e2c7d65
RelapseGFX <albion.metollii@gmail.com>
2025-05-09 12:17:26 +0200

    added js files

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,9 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
+    eleventyConfig.addPassthroughCopy('./src/animation.js');
+    eleventyConfig.addPassthroughCopy('./src/carousel.js');
+    eleventyConfig.addPassthroughCopy('./src/script.js');
 
     return {
         dir: {
